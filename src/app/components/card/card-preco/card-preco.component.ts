6fc4f68178312716ef4480999712cb9449c711f0
Lucas-And3rson <lucasestuda09@gmail.com>
2023-08-20 21:11:58 -0300

    entregando projeto

---
+++ src/app/components/card/card-preco/card-preco.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-preco',
+  templateUrl: './card-preco.component.html',
+  styleUrls: ['./card-preco.component.css']
+})
+export class CardPrecoComponent implements OnInit{
+  @Input()
+  gameType:string ="Digital PS4"
+  @Input()
+  gamePrice:string ="R$399,90"
+
+  constructor() { }
+  ngOnInit(): void{
+
+  }
+}
